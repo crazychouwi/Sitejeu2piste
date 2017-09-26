8b22c33b6d6b47e5882851ac54804d9c5114aa8e
Sandra <ptitbou.d_avril@hotmail.fr>
2017-09-26 11:16:45 +0200

    création page de contact et mise en forme

--- parcoursback.php
+++ parcoursback.php
@@ -1,3 +1,6 @@
+
+
+
 <?php
 session_start();
 
@@ -9,13 +12,31 @@ if ($_SESSION['admin'] == "ok"){
     <head>
       <meta charset="utf-8">
       <title>parcoursback</title>
+      <link rel="stylesheet" href="assets/css/carousel.css">
     </head>
     <body>
+      <div class="enteteAdmin">
+        <form class="" action="index.html" method="post">
+          <input  type="submit" name="bouton" value="Deconnexion">
+        </form>
+      </div>
       <h1>page admin!</h1>
-      <p>
-        faire un bouton de deconnexion qui renvoit sur la page d'accueil </br>
-        penser a inserer la session_destroy
-      </p>
+      <p>penser a inserer la session_destroy</p>
+      <div class="contenu">
+      <?php
+      // connection à la base de donnée
+      require_once 'connection.php';
+
+      $reponse = $bdd->query('SELECT * FROM parcours');
+
+      while($donnees = $reponse->fetch())
+      {
+        echo "<div id='parcours'>
+        <div class='title'><h1>" . $donnees["titre"]."</h1></div>"." "."<div class='text'><p>" . $donnees["texte"]."</p><div><button>Modifier</button><button>Supprimer</button></div></div>
+        </div>";
+      };
+       ?>
+      </div>
     </body>
   </html>
 
